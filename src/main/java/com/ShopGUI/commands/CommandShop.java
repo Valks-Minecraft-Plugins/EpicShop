949bcc9b254e4f8e32876987af03ee9f5b50b6b5
valkyrienyanko <sebastianbelle074@gmail.com>
2019-03-06 18:27:06 -0600

    Init Configs

--- src/main/java/com/ShopGUI/commands/CommandShop.java
+++ src/main/java/com/ShopGUI/commands/CommandShop.java
@@ -6,40 +6,35 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import com.ShopGUI.ShopGUI;
-
+import com.ShopGUI.utils.Utils;
+//sender.sendMessage((String) config.getMapList("shop.shops").get(0).get("tester"));
 public class CommandShop implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 		if (command.getName().equalsIgnoreCase("shop")) {
 			
 			ShopGUI plugin = ShopGUI.plugin;
-			YamlConfiguration config = plugin.getShopConfig();
+			YamlConfiguration shopConfig = plugin.getShopConfig();
+			YamlConfiguration messagesConfig = plugin.getMessagesConfig();
 			
 			if (args.length < 1) {
-				sender.sendMessage("Opening shop...");
+				if (messagesConfig.getBoolean("messages.opening_shop.enabled"))
+					sender.sendMessage(Utils.color(messagesConfig.getString("messages.opening_shop.message")));
 				return true;
 			}
+			
+			//
 			
 			if (args[0].equalsIgnoreCase("help")) {
-				sender.sendMessage("ShopGUIReborn Help"
-						+ "\n"
+				sender.sendMessage(Utils.color("EpicShop Help"
+						+ "\n "
 						+ "\n/shop"
-						+ "\n/shop create [category]");
+						+ "\n/shop create [category]"));
 				return true;
 			}
 			
-			if (args[0].equalsIgnoreCase("create")) {
-				if (args.length < 2) {
-					sender.sendMessage("/shop create [category]");
-					return true;
-				}
-				
-				sender.sendMessage("Created shop category named " + args[1] + "!");
-				
-				//sender.sendMessage((String) config.getMapList("shop.shops").get(0).get("tester"));
-				
-				config.set("shops.food.test", true);
-				
-				plugin.saveShopConfig();
+			if (args[0].equalsIgnoreCase("edit")) {
+				if (messagesConfig.getBoolean("messages.editing_shop.enabled"))
+					sender.sendMessage(Utils.color(messagesConfig.getString("messages.editing_shop.message")));
 				
 				return true;
 			}
